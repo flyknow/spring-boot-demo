7846905cf061cb34efcf2266569fed8104e2ec52
zhuyongfei@supcon.com <Zhuyongfei.01>
2022-04-25 11:14:35 +0800

    修改

---
+++ demo-mybatis-plus/src/main/java/com/flyyon/DemoMyBatisPlusApplication.java
@@ -0,0 +1,18 @@
+package com.flyyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author: Zhuyf
+ * @version: 2022/4/25
+ **/
+@SpringBootApplication
+public class DemoMyBatisPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMyBatisPlusApplication.class, args);
+    }
+
+}
